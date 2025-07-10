aea0343a31e4ed17ba8c7a6b3ceb3705e713dc2a
Salamastik <salamastik@gmail.com>
2025-07-10 11:15:23 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-sudo apt update && install -y git docker.io wget curl 
+sudo apt update && sudo apt install -y git docker.io wget curl 
 sudo snap install --classic code
+
 # Download and install nvm:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
@@ -17,12 +18,13 @@ nvm current # Should print "v22.17.0".
 npm -v # Should print "10.9.2".
 npm install -g @google/gemini-cli
 
-ssh-keygen
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
+git clone git@github.com:Salamastik/open-webui.git
 
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
 sudo apt install -y python3.11 python3.11-venv python3.11-dev
+ssh-keygen
+
